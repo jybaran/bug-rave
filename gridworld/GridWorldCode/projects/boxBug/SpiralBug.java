db91a6ca039895dbfb56e29b243ab99215ad9110
Jenny Baran <jenny.baran@gmail.com>
2014-02-10 22:46:39 -0500

    added spiral bug
    also accidentally deleted BoxBugRunner and had to remake, sorry

--- gridworld/GridWorldCode/projects/boxBug/SpiralBug.java
+++ gridworld/GridWorldCode/projects/boxBug/SpiralBug.java
@@ -18,20 +18,16 @@
 
 import info.gridworld.actor.Bug;
 
-/**
- * A <code>BoxBug</code> traces out a square "box" of a given size. <br />
- * The implementation of this class is testable on the AP CS A and AB exams.
- */
-public class BoxBug extends Bug
+public class SpiralBug extends Bug
 {
     private int steps;
     private int sideLength;
 
     /**
-     * Constructs a box bug that traces a square of a given side length
+     * Constructs a box bug that traces a spiral beginning with a given side length
      * @param length the side length
      */
-    public BoxBug(int length)
+    public SpiralBug(int length)
     {
         steps = 0;
         sideLength = length;
@@ -52,6 +48,7 @@ public class BoxBug extends Bug
             turn();
             turn();
             steps = 0;
+	    sideLength++;
         }
     }
 }
